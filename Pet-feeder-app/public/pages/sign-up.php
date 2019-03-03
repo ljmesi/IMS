1ba350c07b88eb27247385f290963c68d67e031a
yuvaranimasarapu <8208390+yuvaranimasarapu@users.noreply.github.com>
2019-03-03 23:27:42 +0100

    sign up page and captcha updation

--- Pet-feeder-app/public/pages/sign-up.php
+++ Pet-feeder-app/public/pages/sign-up.php
@@ -1,21 +1,38 @@
 <?php include('../../private/initialise.php') ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+</head>
 
+<html>
+<body>
 <div class="container">
     <h3>Sign-up form</h3> 
-    <form action="" method="post">
+    <form action="captcha-validate.php" method="post">
       
       <ul class="flex-outer">
-        <li><label for="username">Username</label> <input type="email" name="username" id="username"
-            placeholder="Use your email address here" require></li>
-        <li><label for="password">Password</label> <input type="password" name="password" id="password" 
-            require ></li>
+        <li><label for="username">Email address</label> <input type="email" name="username" id="username" placeholder="Use your email address here" required></li>
+        <li><label for="password">Password</label> <input type="password" name="password" id="password" required></li>
+      </ul>
+        <label for="captcha">Please Enter the Captcha Text</label><img src='../../private/captcha.php' id="captcha">
+            <input type="text" name="answer" placeholder="Enter captcha here"/>
+                <!-- <input type="submit" value="Submit"/> -->
+        <input type="button" id="reload" value="Reload" />
             <!-- <li><label for="password">Password</label> <input type="password" name="password" id="password" 
             minlength="8" require ></li> -->
-        <li><input type="submit" value="Sign me up!"></li>
-      </ul>
+        <br><input type="submit" value="Sign me up!">
+      
         
     </form>
+    <script>
+        $(function() { // Handler for .ready() called.
+            $('#reload').click(function(){
+                $('#captcha').attr('src', 'captcha.php?' + (new Date).getTime());
+            });
+        });
+    </script>
 </div>
+</body>
+</html>
 
 <?php include(SHARED_PATH . '/footer.php'); ?>

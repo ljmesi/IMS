b7b22ec6512c393901dbf3802a57a820ed4d4988
yuvaranimasarapu <8208390+yuvaranimasarapu@users.noreply.github.com>
2019-03-03 23:32:53 +0100

    additional files needed for captcha to work

---
+++ Pet-feeder-app/private/captcha.php
@@ -0,0 +1,25 @@
+<?php
+/* captcha.php file */
+session_start();
+header("Expires: Tue, 01 Jan 2013 00:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+/*do it not cacheable for it can change between page loads*/
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+$chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$randomString = '';
+for ($i = 0; $i < 5; $i++)
+{
+    $randomString .= $chars[rand(0, strlen($chars)-1)];
+}
+$_SESSION['captcha'] = strtolower( $randomString );
+
+$im = @imagecreatefromjpeg('../../../public/captcha.jpg'); 
+$text_color = imagecolorallocate($im, 233, 14, 91);
+imagestring($im, 6, 16, 5,  $randomString, $text_color);
+header('Content-type: image/jpeg');
+imagejpeg($im);
+imagedestroy($im);
+?>

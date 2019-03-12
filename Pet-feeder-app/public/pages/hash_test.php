20aa6cfde6c5f74ddf282c09097d10993b524f77
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-03-12 03:17:09 +0100

    redirection changes made to login page and hash algorithm updated with md5 for extra security

--- Pet-feeder-app/public/pages/hash_test.php
+++ Pet-feeder-app/public/pages/hash_test.php
@@ -1,8 +1,4 @@
 <?php
-echo password_hash("Ivory_34@",PASSWORD_BCRYPT);
-echo "<br>";
-echo password_hash("Ivory_34@",PASSWORD_BCRYPT);
-echo "<br>";
 echo md5("Blackhawk@1");
 echo "<br>";
 echo md5("Blackhawk@1");
--- Pet-feeder-app/public/pages/login_check.php
+++ Pet-feeder-app/public/pages/login_check.php
@@ -1,26 +1,27 @@
 <?php include('../../private/initialise.php') ?>
 <?php include(SHARED_PATH . '/header.php'); ?>
-<?php 
+<?php
 session_start();
 if ( ! empty( $_POST ) ) {
     if ( isset( $_POST['emailaddr'] ) && isset( $_POST['psw'] ) ) {
         // Getting submitted user data from database
-        $con = new mysqli('localhost', 'root', '', 'Pet_feeder');
+        $con = new mysqli('localhost', 'root', '', 'pet_feeder');
         $stmt = $con->prepare("SELECT email, own_password FROM pet_owner WHERE email= ? LIMIT 1");
         $stmt->bind_param('s', $_POST['emailaddr']);
         $stmt->execute();
         $result = $stmt->get_result();
     	$user = $result->fetch_array(MYSQLI_NUM);
         
-    	// Verify user password and set $_SESSION
-    	if ( md5($_POST['psw']) == $user[1] ) {
-            $_SESSION['user_id'] = $user[0];
-            //echo "test";
-            header('Location: profilepage.php');
-        }
-        else {
+        // Verify user password and set $_SESSION
+        $hash_password = md5($_POST['psw']);
+    	if ( strcmp($hash_password,$user[1]) == 0 ) {
             echo "<h2>Oops, email does not exist with us. Try login again.</h2>";
             echo "<h4><a href='sign-in.php'>Login</a></h4>";
+        }
+        else {
+            $_SESSION['user_id'] = $user[0];
+            echo "test";
+            header('Location: http://localhost/IMS/Pet-feeder-app/index.php');
         }
 
     }

08ce0cdc3f797cd0b7736d9d7918ac32dff18056
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-03-12 02:32:57 +0100

    log-out page updated with session variables for added security

--- Pet-feeder-app/public/pages/log-out.php
+++ Pet-feeder-app/public/pages/log-out.php
@@ -2,6 +2,7 @@
    session_start();
    unset($_SESSION["email"]);
    unset($_SESSION["psw"]);
+   session_destroy();
    
    //echo 'You have cleaned session';
    header('Refresh: 0; URL = sign-in.php');

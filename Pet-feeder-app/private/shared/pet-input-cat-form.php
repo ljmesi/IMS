7e151eb4146572dbdb17484795cfe5a6e22e97a9
Alina Orozco <alina.orozco12@gmail.com>
2019-03-17 11:34:37 -0700

    Changed cat breeds

--- Pet-feeder-app/private/shared/pet-input-cat-form.php
+++ Pet-feeder-app/private/shared/pet-input-cat-form.php
@@ -5,11 +5,51 @@
                 <label for="cat_breed">Cat breed</label>
                 <select id="cat_breed" name="cat_breed" required>
                     <option value="">Select</option>
-                    <option value="Regular Domestic Cat">Regular Domestic Cat</option>
-                    <option value="Large Domestic Cat">Large Domestic Cat</option>
-                    <option value="Persian Cat">Persian Cat</option>
+                    <option value="Abyssinian Cat">Abyssinian Cat</option>
+                    <option value="American Bobtail">American Bobtail</option>
+                    <option value="American Curl">American Curl</option>
+                    <option value="American Shorthair">American Shorthair</option>
+                    <option value="American Wirehair">American Wirehair</option>
+                    <option value="Balinese-Javanese Cat">Balinese-Javanese Cat</option>
+                    <option value="Bengal">Bengal</option>
+                    <option value="Birman">Birman</option>
+                    <option value="Bombay">Bombay</option>
+                    <option value="British Shorthair">British Shorthair</option>
+                    <option value="Burmese">Burmese</option>
+                    <option value="Chartreux">Chartreux</option>
+                    <option value="Cornish Rex">Cornish Rex</option>
+                    <option value="Devon Rex">Devon Rex</option>
+                    <option value="Egyptian Mau">Egyptian Mau</option>
+                    <option value="European Burmese">European Burmese</option>
+                    <option value="Exotic Shorthair">Exotic Shorthair</option>
+                    <option value="Havana Brown">Havana Brown</option>
+                    <option value="Himalayan">Himalayan</option>
+                    <option value="Japanese Bobtail">Japanese Bobtail</option>
+                    <option value="Korat">Korat</option>
+                    <option value="LaPerm">LaPerm</option>
                     <option value="Maine Coon">Maine Coon</option>
-                    <option value="Siamese">Siamese</option>
+                    <option value="Manx">Manx</option>
+                    <option value="Manx Cat">Manx Cat</option>
+                    <option value="Norwegian Forest Cat">Norwegian Forest Cat</option>
+                    <option value="Ocicat">Ocicat</option>
+                    <option value="Oriental">Oriental</option>
+                    <option value="Persian">Persian</option>
+                    <option value="Peterbald">Peterbald</option>
+                    <option value="Pixiebob">Pixiebob</option>
+                    <option value="Ragamuffin">Ragamuffin</option>
+                    <option value="Russian Blue">Russian Blue</option>
+                    <option value="Savannah">Savannah</option>
+                    <option value="Scottish Fold">Scottish Fold</option>
+                    <option value="Selkirk Rex">Selkirk Rex</option>
+                    <option value="Siamese Cat">Siamese Cate</option>
+                    <option value="Siberian">Siberian</option>
+                    <option value="Singapura">Singapura</option>
+                    <option value="Somali">Somali</option>
+                    <option value="Sphynx">Sphynx</option>
+                    <option value="Tonkinese">Tonkinese</option>
+                    <option value="Toyger">Toyger</option>
+                    <option value="Turkish Angora">Turkish Angora</option>
+                    <option value="Turkish Van">Turkish Van</option>
+
                 </select>
             </li>
-
